47c1abc9007b4c375512c622185d56e004ca28d1
Teddy-Hung <tedhungkk@gmail.com>
2021-01-11 17:17:48 -0800

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config
+const express = require('express')
+const massive = require('massive')
+const products_controller = require('./products_controller')
+
+const app = express()
+const { SERVER_PORT, CONNECTION_STRING } = process.env
+
+app.use(express.json())
+
+app.listen(SERVER_PORT, () => console.log(`Server is running on ${SERVER_PORT}`));
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(dbInstance => {
+    app.set('db', dbInstance)
+}).catch(err => console.log(err))
+
+//endpoints
+app.post('/api/products', products_controller.create)
+app.get('/api/products', products_controller.getAllProducts)
+app.get('/api/products/:id', products_controller.getProduct)
+app.put('/api/products/:id', products_controller.update)
+app.delete('/api/products/:id', products_controller.delete)
---
+++ products_controller.js
@@ -0,0 +1,56 @@
+module.exports = {
+    create: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const {name, description, price, image_url} = req.body
+
+        dbInstance.create_product([name, description, price, image_url])
+            .then( () => res.sendStatus(200))
+            .catch( err => {
+                res.status(500).send({errorMessage: 'Failed to create product!'})
+            })
+    },
+    getProduct: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const {id} = req.params
+
+        dbInstance.read_product(id)
+            .then(product => res.status(200).send(product))
+            .catch( err => {
+                res.status(500).send({errorMessage: 'Failed to get product!'})
+            })
+        
+    },
+    getAllProducts: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+
+        dbInstance.read_products()
+        .then(products => res.status(200).send(products))
+        .catch( err => {
+            res.status(500).send({errorMessage: 'Failed to get all products!'})
+        })
+
+    },
+    update: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const { id } = req.params
+        const {desc} = req.query
+
+        dbInstance.update_product( id, desc)
+            .then( () => sendStatus(200) )
+            .catch( err => {
+                res.status(500).send({errorMessage: 'Failed to update product!'})
+                console.log(err)
+            })
+    },
+    delete: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const { id } = req.params
+
+
+        dbInstance.update_product(id)
+            .then(() => sendStatus(200))
+            .catch( err => {
+                res.status(500).send({errorMessage: 'Failed to delete product!'})
+            })
+    }
+}
